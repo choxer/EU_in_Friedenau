da68258330ab98906bd9abe5125e340892c36f5f
choxer <33546322+choxer@users.noreply.github.com>
2026-01-30 06:45:11 +0100

    erste Tests

--- js/map.js
+++ js/map.js
@@ -7,3 +7,13 @@ export function initMap() {
 
   return map;
 }
+
+
+export const euIcon = L.icon({
+  iconUrl: "./assets/Flag_of_Europe.svg",
+  iconSize: [28, 28],
+  iconAnchor: [14, 28],
+  popupAnchor: [0, -28]
+});
+
+
